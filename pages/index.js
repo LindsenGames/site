8b7dfbd649d1033065972e2b3663f53e3aa3e330
Mary Lindsen <mary.lasek@hotmail.com>
2022-07-23 16:02:49 -0300

    ADD JOGOS

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Header from "../components/header";
 import Slider from "../components/slider";
 import Footer from "../components/footer";
+import Jogos from "../components/jogos";
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <Header />
       <Slider />
       <Footer />
+      <Jogos />
     </>
   );
 }
